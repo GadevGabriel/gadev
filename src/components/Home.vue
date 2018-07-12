0b293aff415545ca2c4aef9d3143f13f7450bba4
Gabriel <desenvolvimento@gadev.com.br>
2018-07-12 20:11:31 -0300

    Criação de componente Home e componente de Teste

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  .home Home
+</template>
+
+<script>
+export default {
+  name: 'Home',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+
+</style>
---
+++ src/components/Teste.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  .teste TESTE
+</template>
+
+<script>
+export default {
+  name: 'Teste',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+
+</style>
