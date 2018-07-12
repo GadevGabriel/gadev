cb5526b42e607ee27efe383371b1a2ae3b039d5d
Gabriel <desenvolvimento@gadev.com.br>
2018-07-12 20:12:05 -0300

    Criação de rotas e configuração de Webpack para SASS e PUG

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -74,6 +74,23 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              indentedSyntax: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-plain-loader'
       }
     ]
   },
